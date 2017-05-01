a804ebda1e85a2695c4ffc6a9142046dee50563e
john dowd and roger tobias <john@>
2017-05-01 09:47:52 -0700

    intial setup up of files and js, css and html.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $("#questions").submit(function(event) {
+    event.preventDefault();
+    //
+    // var nameInput = $("input#name").val();
+    // var questionOne = parseInt($("#questionOne").val());
+    // var questionTwo = parseInt($("#questionTwo").val());
+    // var questionThree =  parseInt($("#questionThree").val());
+    // var questionFour = parseInt($("#questionFour").val());
+    // var questionFive =  parseInt($("#questionFive").val());
+    // var result = questionOne + questionTwo + questionThree + questionFour + questionFive;
+    //
+    //
+    // if(!isNaN(result)) {
+    //   if (result <= 4) {
+    //     $(".option").hide();
+    //     $("#css").show();
+    //   } else if (result <= 5) {
+    //     $(".option").hide();
+    //     $("#c").show();
+    //   } else if (result <= 8) {
+    //     $(".option").hide();
+    //     $("#java").show();
+    //   }
+    } else {
+      alert('Please select all values!')
+    }
+    //
+    // $(".name").text(nameInput);
+  });
+});
